3ce571b8d524253a08f34afd5a090246babd5ebf
tamakoshi <tamakosiy@gmail.com>
2023-05-27 08:24:31 +0100

    fix webhook verify issue and status code issue

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import functions_framework
 
 @functions_framework.http
 def main(request):
-    reply = json_dump(request)
-    text = reply['events'][0]['message']['text']
-    reply_token = reply['events'][0]['replyToken']
-    # chatGPTからの返信
-    completions = chatGPT_api(text)
-    # LINEbotに返信
-    status_code = line_api(reply_token, completions)
+    try:
+        reply = json_dump(request)
 
-    if status_code == 200:
-        print("Message replied successfully.")
-    else:
-        print(f"Message reply failed with status code: {status_code}")
-    return None
+        # Webhook verify
+        if reply['events'] == []:
+            return ({}, 200)
 
+        text = reply['events'][0]['message']['text']
+        reply_token = reply['events'][0]['replyToken']
+        # chatGPTからの受信
+        completions = chatGPT_api(text)
+        # LINEbotに送信
+        status_code = line_api(reply_token, completions)
 
-chatGPT_api('who are you?')
+        if status_code == 200:
+            print("Message replied successfully.")
+        else:
+            print(f"Message reply failed with status code: {status_code}")
+        return '200'
+    except Exception as e:
+        print(str(e))
+        return '500'
